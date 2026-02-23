3be5d549da030ff798bbe1968f512dc3429e4894
xieshi-up <3773793283@qq.com>
2026-02-23 09:55:17 +0000

    fix: add PagesFunction type to functions route

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,4 +1,6 @@
-export const onRequest = async (context) => {
+import type { PagesFunction } from '@cloudflare/workers-types';
+
+export const onRequest: PagesFunction = async (context) => {
   const { default: worker } = await import('../.open-next/worker');
   return worker.fetch(context.request, context.env, context);
 };
